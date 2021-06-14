53d8dc56fdaf8a0db8e569823fbaf1482601466a
AnteKevo <antonio.kevo@gmail.com>
2021-06-14 11:30:13 +0200

    WIP

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
 from PIL import Image
+import os
 
-ASCII_CHARS = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1\{\}[]?-_+~<>i!lI;:,\"^`'. "
+ASCII_CHARS = "@%#*+=-:. "
 
-def main(new_width = 150):
+def main():
+    t_line = os.get_terminal_size().lines - 1
     path = input("Enter image path: ")
-    img = Image.open(path)
-    new_image_data = pixels_to_ascii(grayify(resize(img, new_width)))
-    ascii_image = "\n".join([new_image_data[i:i+new_width] for i in range(0, len(new_image_data), new_width)])
-    f = open("output.txt", "w")
-    f.write(ascii_image)
-    f.close()
+    try:
+        img = Image.open(path)
+        new_img, new_w = resize(img, t_line)
+        new_img_data = pixels_to_ascii(grayify(new_img))
+        ascii_image = "\n".join([new_img_data[i:i+new_w] for i in range(0, len(new_img_data), new_w)])
+        #f = open("output.txt", "w")
+        #f.write(ascii_image)
+        #f.close()
+        print(ascii_image)
+        img.close()
+    except FileNotFoundError as fnf:
+        print(fnf)
     
-def resize(img, new_width):
-    width, height = img.size
-    return img.resize((new_width, int(new_width * (height / width))))
+    except PIL.UnidentifiedImageError as err:
+        print(err)
+    
+def resize(img, new_h):
+    w, h = img.size
+    ratio = w / h
+    new_w = int(new_h * ratio)
+    return (img.resize((new_w, new_h)), new_w)
 
 def grayify(image):
     pixels = image.load()
@@ -28,7 +41,7 @@ def grayify(image):
 
 def pixels_to_ascii(image):
     pixels = image.getdata()
-    return "".join([ASCII_CHARS[pixel[0] // 4] for pixel in pixels])
+    return "".join([ASCII_CHARS[pixel[0] // 26] for pixel in pixels])
 
 
 if __name__ == '__main__':

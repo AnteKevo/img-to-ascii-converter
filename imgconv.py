dd736a8526c7aa00b758ed9c7e418ff806c1b24c
AnteKevo <antonio.kevo@gmail.com>
2021-06-15 19:22:40 +0200

    Added color support using ANSI Escape Codes and negative image support

--- imgconv.py
+++ imgconv.py
@@ -3,8 +3,16 @@ from PIL import Image
 import math
 
 # Retrieved from http://paulbourke.net/dataformats/asciiart/
-ASCII_CHARS_S = "@%#*+=-:. "
-ASCII_CHARS_C = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{\}[]?-_+~<>i!lI;:,\"\^`'. "        
+ASCII_CHARS_S = " .:-=+*#%@"
+ASCII_CHARS_C = " .'`^\",:;Il!i><~+_-?][}{1)(|/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
+
+class RGBPixel:
+    def __init__(self, r, g, b, complex, negative):
+        self.r = 255 - r if negative else r
+        self.g = 255 - g if negative else g
+        self.b = 255 - b if negative else b
+        self.grayscale = int(0.299*r + 0.587*g + 0.114*b)
+        self.value = ASCII_CHARS_C[int((self.grayscale) // (256 / 70))] if complex else ASCII_CHARS_S[int(self.grayscale // (256 / 10))]
 
 def resize(img):
     t_size = os.get_terminal_size()
@@ -20,8 +28,15 @@ def resize(img):
     return img.resize((a_w, a_h), Image.LANCZOS)
 
 
-def convert_to_ascii(img, complex, negative):
+def convert_to_ascii(img, complex, negative, color):
+    if color:
+        if img.mode == "RGB" or img.mode == "RGBA":
+            pixels = Image.Image.getdata(img)
+            ascii_pixels = [RGBPixel(*pixel[:3], complex, negative) for pixel in pixels]
+            return list(map(lambda a_c: f"\033[38;2;{a_c.r};{a_c.g};{a_c.b}m{a_c.value}\033[0m", ascii_pixels))
+        else:
+            print("Error! Image is not in RGB format")
     pixels = Image.Image.getdata(img.convert("L"))
     if negative:
-        return ''.join([ASCII_CHARS_C[math.ceil((255 - pixel) // (256 / 70))] if complex else ASCII_CHARS_S[math.ceil((255 - pixel) // (256 / 10))] for pixel in pixels])
-    return ''.join([ASCII_CHARS_C[math.ceil(pixel // (256 / 70))] if complex else ASCII_CHARS_S[math.ceil(pixel // (256 / 10))] for pixel in pixels])
+        return [ASCII_CHARS_C[int((255 - pixel) // (256 / 70))] if complex else ASCII_CHARS_S[int((255- pixel) // (256 / 10))] for pixel in pixels]
+    return [ASCII_CHARS_C[int(pixel // (256 / 70))] if complex else ASCII_CHARS_S[int(pixel // (256 / 10))] for pixel in pixels]
--- main.py
+++ main.py
@@ -11,6 +11,7 @@ parser = argparse.ArgumentParser(description="Converts images into ASCII art")
 parser.add_argument("input", type=str, help="The image you wish to convert")
 parser.add_argument("-o", "--output", type=str, help="Saves the ASCII image in a .txt-file")
 parser.add_argument("-n", "--negative", help="Creates a negative ASCII image", action="store_true")
+parser.add_argument("-c", "--color", help="Attempts to color the ASCII characters if the image supports RGB", action="store_true")
 parser.add_argument("-C", "--complex", help="Uses a longer ASCII sequence which represents 70 levels of gray", action="store_true")
 args = parser.parse_args()
 
@@ -29,8 +30,8 @@ def main():
             sys.exit("Error: Unable to locate file")
     
     img = resize(img)
-    ascii_art = convert_to_ascii(img, args.complex, args.negative)
-    result = '\n'.join([ascii_art[i:i+img.size[0]] for i in range(0, len(ascii_art), img.size[0])])
+    ascii_art = convert_to_ascii(img, args.complex, args.negative, args.color)
+    result = "\n".join([''.join(ascii_art[i:i+img.size[0]]) for i in range(0, len(ascii_art), img.size[0])])
 
     if args.output:
         if args.output[-4:] == ".txt":
